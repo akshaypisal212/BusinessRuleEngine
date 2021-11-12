ebdc550115771f9801c846a056b78643245069d3
Akshay Pisal <akshay.pisal@hotmail.com>
2021-11-12 18:12:33 +0530

    Added information log

--- PaymentMS/Rules/Royalty.cs
+++ PaymentMS/Rules/Royalty.cs
@@ -2,14 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using PaymentMS.Enums;
 
 namespace BusinessRuleEngine
 {
     public class Royalty : IRule
     {
+        public Royalty(ILogger<Royalty> logger)
+        {
+            Logger = logger;
+        }
+
+        public ILogger<Royalty> Logger { get; }
+
         public string Execute(PaymentContext paymentContext)
         {
+            this.Logger.LogInformation($"Royalty rule executed for the product type {0}", paymentContext.ProductType.ToString());
             return $"Royalty packaging slip generated for the product {paymentContext.ProductType}.";
         }
 
